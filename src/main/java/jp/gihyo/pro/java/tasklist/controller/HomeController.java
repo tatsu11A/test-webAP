3303cfd81497779b128ec48ed9196b2a833f93cb
Tatsuya Asagoshi <asakoshi@insightech.co.jp>
2026-03-19 17:16:04 +0900

    UI修正＆コメントアウト

--- src/main/java/jp/gihyo/pro/java/tasklist/controller/HomeController.java
+++ src/main/java/jp/gihyo/pro/java/tasklist/controller/HomeController.java
@@ -2,19 +2,41 @@ package jp.gihyo.pro.java.tasklist.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
         record TaskItem(String id, String task, String deadline, boolean done) {
+            // タスクアイテム情報を複数保持するレコードクラス
     }
     private List<TaskItem> taskItems = new ArrayList<>();
+    //ArrayListクラスのインスタンスを生成して、タスクアイテムを格納するためのフィールドを用意。
+
+    // タスクアイテムの 一覧表示のメソッド
+    /*
+        コントローラーで保持しているタスク一覧をModel経由でビューに渡し、
+        Thymeleaf側で表示できる状態にしてからhome画面を返しています。
+    */
     @GetMapping("/list")
     String listItem(Model model){
-        model.addAttribute("taskList", taskItems);
+        model.addAttribute("TaskList", taskItems);
+        //ModelクラスのaddAttributeメソッドを使って、taskItemsをビューに渡すためのTaskList属性を追加しています。
         return "home";
+        //htmlファイルのhomeを返す
+    }
+
+    // タスクアイテムを追加機能のメソッド
+    @GetMapping("/add")
+    String addItem(@RequestParam("task") String task, @RequestParam("deadline") String deadline) {        
+        String id = UUID.randomUUID().toString() .substring(0,8);
+        TaskItem item = new TaskItem(id, task, deadline, false);
+        taskItems.add(item);
+
+        return "redirect:/list";
     }
 }
